28c7ff2bcf21fe47984b8b25b47a01a11c43d416
Gamecrafter <gamecraftervt@gmail.com>
2016-06-22 15:31:42 -0700

    Remove test values

--- dlup.py
+++ dlup.py
@@ -4,17 +4,7 @@ maxThreads = 10
 #How many times should each thread loop?
 loopsPerThread = 25
 #What do you want to use dlup for? Specify absolute URLs of your targets
-targets = [
-    "http://forums.pocketmine.net/plugins/mytag.1001/download?version=2090",
-    "http://forums.pocketmine.net/plugins/imanager.1039/download?version=2066",
-    "http://forums.pocketmine.net/plugins/globalshield.1086/download?version=2034",
-    "http://forums.pocketmine.net/plugins/planb.1128/download?version=2988",
-    "http://forums.pocketmine.net/plugins/easymessages.1208/download?version=2983",
-    "http://forums.pocketmine.net/plugins/blockfreezer.1209/download?version=3098",
-    "http://forums.pocketmine.net/plugins/skintools.1364/download?version=2984",
-    "http://forums.pocketmine.net/plugins/restartme.1509/download?version=2907",
-    "http://forums.pocketmine.net/plugins/queryfacade.1621/download?version=2970"
-]
+targets = []
 #===END CONFIG===
 
 import threading
